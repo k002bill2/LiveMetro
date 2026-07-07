94a340e2b44e7487759bcd82a4d989972dd1ea35
002bill <k002bill2@gmail.com>
2026-07-07 22:09:59 +0900

    feat(guidance): 열차 선택 바텀시트(TrainSelectSheet)

--- src/components/guidance/index.ts
+++ src/components/guidance/index.ts
@@ -3,3 +3,5 @@ export { GuidanceNowCard } from './GuidanceNowCard';
 export { GuidanceStepRow } from './GuidanceStepRow';
 export type { GuidanceStepStatus } from './GuidanceStepRow';
 export { GuidanceControls } from './GuidanceControls';
+export { TrainSelectSheet } from './TrainSelectSheet';
+export type { TrainSelectSheetProps } from './TrainSelectSheet';
